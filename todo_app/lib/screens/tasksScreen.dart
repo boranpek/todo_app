cc61af918350c314560ae686278c96ee8fab0102
boranpek <boran.pekel@ozu.edu.tr>
2021-01-20 16:33:33 +0300

    refactor the code

--- todo_app/lib/screens/tasksScreen.dart
+++ todo_app/lib/screens/tasksScreen.dart
@@ -20,17 +20,7 @@ class _TasksScreenState extends State<TasksScreen> {
       extendBodyBehindAppBar: true,
       body: Stack(
         children: [
-          Container(
-            height: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF283eaf),
-                    Color(0xFF00d4ff)
-                  ]
-              ),
-            ),
-          ),
+          buildBackground(),
           Positioned(
             top:20.0,
             height: 200.0,
@@ -47,62 +37,17 @@ class _TasksScreenState extends State<TasksScreen> {
               ),
             ),
           ),
-
           Positioned(
             top: 300.0,
             left: 0.0,
             right: 0.0,
             bottom: 0.0,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(35.0),
-              ),
-              child: ListView(
-                children: [
-                  SizedBox(height: 15.0,),
-                  Column(children:_getItems(),
-                  ),
-                ],
-              ),
-            ),
+            child: buildCard(),
           ),
           Positioned(
             top: 315.0,
             left: 20.0,
-            child: PopupMenuButton<String>(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(25.0),
-                    ),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-                child: Text(
-                  "$_filterValue",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              onSelected: (String value) {
-                setState(() {
-                  _filterValue = value;
-                });
-              },
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: 'All',
-                  child: Text('All'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'Done',
-                  child: Text('Done'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'Undone',
-                  child: Text('Undone'),
-                ),
-              ],
-            ),
+            child: buildPopupMenuButton(),
           ),
         ],
       ),
@@ -111,6 +56,72 @@ class _TasksScreenState extends State<TasksScreen> {
           tooltip: 'Add Item',
           child: Icon(Icons.add)),
     );
+  }
+
+  PopupMenuButton<String> buildPopupMenuButton() {
+    return PopupMenuButton<String>(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25.0),
+                  ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              child: Text(
+                "$_filterValue",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            onSelected: (String value) {
+              setState(() {
+                _filterValue = value;
+              });
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'All',
+                child: Text('All'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Done',
+                child: Text('Done'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Undone',
+                child: Text('Undone'),
+              ),
+            ],
+          );
+  }
+
+  Card buildCard() {
+    return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(35.0),
+            ),
+            child: ListView(
+              children: [
+                SizedBox(height: 15.0,),
+                Column(children:_getItems(),
+                ),
+              ],
+            ),
+          );
+  }
+
+  Container buildBackground() {
+    return Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color(0xFF283eaf),
+                  Color(0xFF00d4ff)
+                ]
+            ),
+          ),
+        );
   }
 
   @override
@@ -132,94 +143,106 @@ class _TasksScreenState extends State<TasksScreen> {
     return Row(
       children: [
         Flexible(
-          child: CheckboxListTile(
-            title: Text(item.getTaskTitle, style: TextStyle(decoration: item.getIsDone ? TextDecoration.lineThrough : null),),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: item.getIsDone,
-            onChanged: (bool value) {
-              setState(() {
-                item.setIsDone(value);
-                _db.updateTaskItem(item);
-              });
-            },
-            activeColor: Colors.green,
-            checkColor: Colors.black,
-          ),
+          child: checkboxListTile(item),
         ),
-        IconButton(
-          icon: Icon(Icons.update),
-          onPressed: () {
-            setState(() {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
-                      title: const Text('Update a task in your list'),
-                      content: TextField(
-                        controller: _textFieldController,
-                        decoration: const InputDecoration(hintText: 'Enter task here'),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          child: const Text('Update'),
-                          onPressed: () {
-                            setState(() {
-                              item.setTaskTitle(_textFieldController.text);
-                              _db.updateTaskItem(item);
-                              Fluttertoast.showToast(
-                                msg: "Task is Updated!",
-                                toastLength: Toast.LENGTH_LONG,
-                                gravity: ToastGravity.BOTTOM,
-                                backgroundColor: Colors.green,
-                                textColor: Colors.white,
-                              );
-                              Navigator.of(context).pop();
-                            });
-                            _textFieldController.clear();
-                          },
-                        ),
-                        TextButton(onPressed: () {Navigator.pop(context);}, child: Text("Cancel"))
-                      ],
-                    );
-                  });
-            });
-          },
-        ),
-        IconButton(
-          icon: Icon(Icons.restore_from_trash),
-          onPressed: () {
-            setState(() {
-              showDialog(
-                context: context,
-                builder: (_) => AlertDialog(
-                  title: Text("Are you sure to delete?"),
-                  actions: [
-                    TextButton(onPressed: () {
-                      setState(() {
-                        _db.removeTaskItem(item);
-                        Fluttertoast.showToast(
-                          msg: "Task is Deleted!",
-                          toastLength: Toast.LENGTH_LONG,
-                          gravity: ToastGravity.BOTTOM,
-                          backgroundColor: Colors.green,
-                          textColor: Colors.white,
-                        );
-                        Navigator.pop(context);
-                      });
-                    }, child: Text("Yes")),
-                    TextButton(onPressed: () {Navigator.pop(context);}, child: Text("No"))
-                  ],
-                  backgroundColor: Colors.white,
-                  elevation: 35.0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
-                ),
-              );
-            });
-          },
-        ),
+        updateButton(item),
+        deleteButton(item),
       ],
     );
+  }
+
+  IconButton deleteButton(TaskItem item) {
+    return IconButton(
+        icon: Icon(Icons.restore_from_trash),
+        onPressed: () {
+          setState(() {
+            showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                title: Text("Are you sure to delete?"),
+                actions: [
+                  TextButton(onPressed: () {
+                    setState(() {
+                      _db.removeTaskItem(item);
+                      Fluttertoast.showToast(
+                        msg: "Task is Deleted!",
+                        toastLength: Toast.LENGTH_LONG,
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: Colors.green,
+                        textColor: Colors.white,
+                      );
+                      Navigator.pop(context);
+                    });
+                  }, child: Text("Yes")),
+                  TextButton(onPressed: () {Navigator.pop(context);}, child: Text("No"))
+                ],
+                backgroundColor: Colors.white,
+                elevation: 35.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
+              ),
+            );
+          });
+        },
+      );
+  }
+
+  IconButton updateButton(TaskItem item) {
+    return IconButton(
+        icon: Icon(Icons.update),
+        onPressed: () {
+          setState(() {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
+                    title: const Text('Update a task in your list'),
+                    content: TextField(
+                      controller: _textFieldController,
+                      decoration: const InputDecoration(hintText: 'Enter task here'),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Update'),
+                        onPressed: () {
+                          setState(() {
+                            item.setTaskTitle(_textFieldController.text);
+                            _db.updateTaskItem(item);
+                            Fluttertoast.showToast(
+                              msg: "Task is Updated!",
+                              toastLength: Toast.LENGTH_LONG,
+                              gravity: ToastGravity.BOTTOM,
+                              backgroundColor: Colors.green,
+                              textColor: Colors.white,
+                            );
+                            Navigator.of(context).pop();
+                          });
+                          _textFieldController.clear();
+                        },
+                      ),
+                      TextButton(onPressed: () {Navigator.pop(context);}, child: Text("Cancel"))
+                    ],
+                  );
+                });
+          });
+        },
+      );
+  }
+
+  CheckboxListTile checkboxListTile(TaskItem item) {
+    return CheckboxListTile(
+          title: Text(item.getTaskTitle, style: TextStyle(decoration: item.getIsDone ? TextDecoration.lineThrough : null),),
+          controlAffinity: ListTileControlAffinity.leading,
+          value: item.getIsDone,
+          onChanged: (bool value) {
+            setState(() {
+              item.setIsDone(value);
+              _db.updateTaskItem(item);
+            });
+          },
+          activeColor: Colors.green,
+          checkColor: Colors.black,
+        );
   }
 
   Future<AlertDialog> _displayDialog(BuildContext context) async {
